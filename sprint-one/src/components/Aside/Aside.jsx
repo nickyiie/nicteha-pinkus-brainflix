c5a1a438b505f66b66873dffa995f4cdb97ea370
Nicteha Pinkus <nictehapinkus@gmail.com>
2021-02-28 20:58:47 -0800

    Final Commit Sprint One

--- sprint-one/src/components/Aside/Aside.jsx
+++ sprint-one/src/components/Aside/Aside.jsx
@@ -4,7 +4,7 @@ function Aside ({videos, clickEvent, videoId}){
   return (
     <section className='aside'>
       <h2 className='aside__title'>NEXT VIDEO</h2>
-      {videos.slice(1, -1).map((nextVideo) => {
+      {videos.slice(1).map((nextVideo) => {
           return (
             <div onClick={() => clickEvent(nextVideo.id)} className='aside__video-box' key={nextVideo.id} id={nextVideo.id} >
               <video className='aside__preview' poster={nextVideo.image} alt='video preview'></video>
